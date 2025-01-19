4e64260fc0f77193cdf9410a762678d42c8fb6a6
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-01-19 17:25:12 -0600

    Ship class constructor adjusted to create ship size

--- src/js/ship-class.js
+++ src/js/ship-class.js
@@ -1,27 +1,69 @@
 'use strict';
-'use strict';
 
 class Ship {
-	#orientation;
-	#size;
+  #y; // Initial Position
+  #x;
+  #orientation;
+  #size;
+  #shipArr; // Array representing the ship's positions
 
-	constructor(orientation, size){
-		this.orientation = orientation;
-		this.size = size;
-	}
+  constructor(y, x, orientation, size) {
+    this.y = y;
+    this.x = x;
+    this.orientation = orientation;
+    this.size = size;
+    this.#calculateShipArr(); 
+  }
+  set y(y) {
+    this.#y = y;
+    this.#calculateShipArr(); 
+  }
+  set x(x) {
+    this.#x = x;
+    this.#calculateShipArr(); 
+  }
+  set orientation(orientation) {
+    this.#orientation = orientation;
+    this.#calculateShipArr(); 
+  }
+  set size(size) {
+    this.#size = size;
+    this.#calculateShipArr(); 
+  }
 
-	set orientation(orientation){
-		this.#orientation = orientation;
-	}
-	set size(size) {
-		this.#size = size;
-	}
+  get y() {
+    return this.#y;
+  }
+  get x() {
+    return this.#x;
+  }
+  get orientation() {
+    return this.#orientation;
+  }
+  get size() {
+    return this.#size;
+  }
+  get shipArr() {
+    return this.#shipArr; 
+  }
 
-	get orientation() {
-		return this.#orientation;
-	}
-	get size() {
-		return this.#size;
-	}
+  #calculateShipArr() {
+    const shipArray = [];
+    for (let i = 0; i < this.#size; i++) {
+      if (this.#orientation === 'horizontal') {
+        shipArray.push([this.#x + i, this.#y]); 
+      } else if (this.#orientation === 'vertical') {
+        shipArray.push([this.#x, this.#y + i]); 
+      }
+    }
+    this.#shipArr = shipArray;
+  }
 }
+
+// Example Usage
+const myShip = new Ship(2, 3, 'horizontal', 4);
+console.log(myShip.shipArr); // Output: [[3, 2], [4, 2], [5, 2], [6, 2]]
+
+myShip.orientation = 'vertical';
+console.log(myShip.shipArr); // Output: [[3, 2], [3, 3], [3, 4], [3, 5]]
 
