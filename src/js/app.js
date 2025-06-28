919c06f323b93e28543c9b909ea00459afe00712
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-06-28 15:08:19 -0500

    Rotate key fixed, duplicate ships removed

--- src/js/app.js
+++ src/js/app.js
@@ -132,13 +132,12 @@ const occupiedCells = new Set();
 let boatLength;
 let orientation;
 let isPlacingShips = true;
-const usedBoats = [];
 let selectedShip = null;
-const carrier = new Ship("Carrier", "horizontal", 5);
-const battleship = new Ship("Battleship", "horizontal", 4);
-const cruiser = new Ship("Cruiser", "horizontal", 3);
-const submarine = new Ship("Submarine", "horizontal", 3);
-const destroyer = new Ship("Destroyer", "horizontal", 2);
+const carrier = new Ship("Carrier", "vertical", 5);
+const battleship = new Ship("Battleship", "vertical", 4);
+const cruiser = new Ship("Cruiser", "vertical", 3);
+const submarine = new Ship("Submarine", "vertical", 3);
+const destroyer = new Ship("Destroyer", "vertical", 2);
 
 const shipsByName = {
   carrier,
@@ -147,6 +146,7 @@ const shipsByName = {
   submarine,
   destroyer
 };
+const placedShips = {};
 
 const shipMenu = select('.ship-menu');
 
@@ -163,22 +163,32 @@ for(let row = 0; row < gridSize; row++) {
   }
 }
 function tryPlaceShip(startRow, startCol, shipChoice) {
-  if(shipChoice === null) {
+  if(!shipChoice) {
     alert("You must choose a ship type.");
     return;
   }
+  const shipName = shipChoice.name;
   const positions = [];
+
   for(let i = 0; i < shipChoice.size; i++) {
     let row = startRow;
     let col = startCol;
-    if(shipChoice.orientation === 'horizontal') row += i;
-    if(shipChoice.orientation === 'vertical') col += i;
+    if(shipChoice.orientation === 'horizontal') col += i;
+    if(shipChoice.orientation === 'vertical') row += i;
     const key = `${row}, ${col}`;
     if(row >= gridSize ||  col >= gridSize || occupiedCells.has(key)) {
       alert('Cannot place boat here!');
       return;
     }
     positions.push(key);
+  }
+  if(placedShips[shipName]) {
+    placedShips[shipName].forEach(key => {
+      occupiedCells.delete(key);
+      const [r,c] = key.split(',').map(Number);
+      const cell = select(`.grid-cell[data-row="${r}"][data-col="${c}"]`);
+      if(cell) removeClass(cell,'occupied');
+    });
   }
   positions.forEach((key) => {
     occupiedCells.add(key);
@@ -188,6 +198,7 @@ function tryPlaceShip(startRow, startCol, shipChoice) {
       addClass(cell, 'occupied');
     }
   });
+  placedShips[shipName] = positions;
 }
 
 listen('change', shipMenu, (e)=>{
@@ -200,14 +211,15 @@ listen('change', shipMenu, (e)=>{
     alert("No ship selected");
   }
 });
-listen('keydown', document, (e) => {
+listen('keydown', this, (e) => {
   if (e.key === 'r' || e.key === 'R') {
+    console.log("Key pressed");
     if (selectedShip) {
+      console.log("ship present");
       if(selectedShip.orientation === 'horizontal') {
-        selectedShip.orientation === 'vertical';
-      }
-      if(selectedShip.orientation === 'horizontal') {
-        selectedShip.orientation === 'vertical';
+        selectedShip.orientation = 'vertical';
+      } else if(selectedShip.orientation === 'vertical') {
+        selectedShip.orientation = 'horizontal';
       }
     }
   }
